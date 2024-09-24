f1ec482cf2c8085252f8096045e9867bbe7c93d4
Brandon Odiwuor <brandon.odiwuor@gmail.com>
2024-09-24 10:24:21 +0300

    wallet: 'Filter' out 'send' addresses from 'listreceivedby*'

--- src/wallet/rpc/transactions.cpp
+++ src/wallet/rpc/transactions.cpp
@@ -141,6 +141,8 @@ static UniValue ListReceived(const CWallet& wallet, const UniValue& params, cons
     const auto& func = [&](const CTxDestination& address, const std::string& label, bool is_change, const std::optional<AddressPurpose>& purpose) {
         if (is_change) return; // no change addresses
 
+        if (!wallet.IsMine(address)) return; // no send addresses
+
         auto it = mapTally.find(address);
         if (it == mapTally.end() && !fIncludeEmpty)
             return;
